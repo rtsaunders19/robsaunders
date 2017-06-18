455a16cd49715d0126765022e44f0f4577db9dfb
Robert Saunders <rtsaunders19@gmail.com>
2017-06-18 16:52:31 -0500

    side nav bar, background, code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 
+
+	$('body').show();
+
+
 $('.email-symbol').on('click', function() {
 		alert('rtsaunders19@gmail.com');
 })
 
+$(window).scroll(function () {
+	openNav();
+});
+
+	function openNav() {
+		document.getElementById("mySidenav").style.width = "110px";
+	}
 
 
 
