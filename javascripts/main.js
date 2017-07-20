5f0784fd026d71fb6743ac8f63d37d1d68c1c290
Robert Saunders <rtsaunders19@gmail.com>
2017-07-20 11:56:06 -0500

    cname

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
 
 $(window).scroll(function () {
-	openNav();
+
 });
-
-	function openNav() {
-		document.getElementById("mySidenav").style.width = "110px";
-	}
-
-
 
 
 })
